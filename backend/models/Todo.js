8ca9b64cb489ba79e4aabc3c5f9bf65b3f8e0eb0
Rian R.B.P. Santana <rianrbps@gmail.com>
2025-05-29 11:59:48 -0700

    added user requirement field to the todo model

--- backend/models/Todo.js
+++ backend/models/Todo.js
@@ -4,7 +4,11 @@ const Schema = mongoose.Schema;
 const TodoSchema = new Schema({ // schema names are capitalized 4 best practice
     task: String,
     status: String,
-    createdAt: { type:Date, default:Date.now } // timestamps
+    createdAt: { type:Date, default:Date.now }, // timestamps
+    userId: {
+        type: String,
+        required: true
+    }
 })
 
 const Todo = mongoose.model('Todo', TodoSchema); // allows mongoose interaction to mongodb
